db7dc4b26d9bf3d0cf89c62c3737d5d5fce207a9
vaibh <vaibh@LAPTOP-7CLDGMGB>
2020-02-27 10:23:53 +0530

    code added second add

--- src/p1/NewWork.java
+++ src/p1/NewWork.java
@@ -1,10 +1,13 @@
 package p1;
-
+import java.util.*;
 public class NewWork {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 // mt new work
+		
+		List test = new ArrayList();
+		System.out.println(test.size());
 	}
 
 }
